5c8bebf37faf42b597559ea47b58943495777686
gitjibl <871048617@qq.com>
2021-01-22 16:26:17 +0800

    新增 定时模块 公共模块

---
+++ sysam_common/src/test/java/com/zxy/sysam_common/SysamCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxy.sysam_common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysamCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
