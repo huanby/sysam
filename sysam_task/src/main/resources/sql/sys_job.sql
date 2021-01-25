2eb2c113e9e7be58356c3f002b29d1a42bd845db
gitjibl <871048617@qq.com>
2021-01-25 17:05:38 +0800

    数据库sql

---
+++ sysam_task/src/main/resources/sql/sys_job.sql
@@ -0,0 +1,43 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : MySQL
+ Source Server Type    : MySQL
+ Source Server Version : 80015
+ Source Host           : 127.0.0.1:3306
+ Source Schema         : sysam
+
+ Target Server Type    : MySQL
+ Target Server Version : 80015
+ File Encoding         : 65001
+
+ Date: 25/01/2021 17:00:22
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for sys_job
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_job`;
+CREATE TABLE `sys_job`  (
+  `JOB_ID` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '任务id',
+  `BEAN_NAME` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'spring bean名称',
+  `GROUP` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '任务组名称',
+  `METHOD_NAME` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '方法名',
+  `PARAMS` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '参数',
+  `CRON_EXPRESSION` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'cron表达式',
+  `STATUS` int(2) NOT NULL COMMENT '任务状态  0：正常  1：暂停',
+  `REMARK` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '备注',
+  `CREATE_TIME` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`JOB_ID`) USING BTREE,
+  INDEX `sys_job_create_time`(`CREATE_TIME`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '定时任务表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_job
+-- ----------------------------
+INSERT INTO `sys_job` VALUES (1, 'testTask', '', 'test', 'mrbird', '0/1 * * * * ?', 1, '有参任务调度测试~~', '2018-02-24 16:26:14');
+
+SET FOREIGN_KEY_CHECKS = 1;
